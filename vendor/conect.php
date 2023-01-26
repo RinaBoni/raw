dccdf85bcb2d5121d5de8dda054972aeb128bea6
RinaBoni <brisovae@mail.ru>
2023-01-27 01:50:13 +0900

    пиздец

--- vendor/conect.php
+++ vendor/conect.php
@@ -10,7 +10,16 @@
 
     try{
         $pdo = new PDO("$driver:host=$host;dbname=$db_name;charset=$charset",$db_user, $db_pass, $options);
+        
+        if (isset($_COOKIE['page_visit'])){
+            setcookie('page_visit', ++$_COOKIE['page_visit'], time() + 5);
+        }else{
+            setcookie('page_visit', 1, time() + 5);
+            $_COOKIE['page_visit'] = 1;
+        }
+
     }catch(PDOException $e){
         die("не могу");
     }
+
 ?>
--- vendor/reg.php
+++ vendor/reg.php
@@ -1,20 +1,37 @@
 <?php
+    //побключаем бд
     require_once 'conect.php';
     
 
+    //создаем переменные, заносим в них то что подучили из regestration.html, предворительно удалив пробелы
     $login = trim($_POST['login']);
     $email = trim($_POST['email']);
     $password = trim($_POST['password']);
     $password_confirm = trim($_POST['password_confirm']);
 
+    //проверка: заполнены все поля, совпадают значения password и password_confirm
     if( !empty($login) && !empty($email) && !empty($password) && !empty($password_confirm)){
         if ($password === $password_confirm){
             
+            //проверка на существование пользователя
+            $sql_check = 'SELECT EXISTS(SELECT login FROM users WHERE login = :login)';
+            
+            $stmt_check = $pdo->prepare($sql_check);
+            $stmt_check->execute([':login' => $login]);
+
+            if($stmt_check->fetchColumn()){
+                die('Пользователь с таким логином существует');
+            }
+
+            
+            //хеширование пароля
             $password = password_hash($password, PASSWORD_DEFAULT);
             
+            //sql запрос
             $sql = 'INSERT INTO users(login, email, password) VALUES(:login, :email, :password)';
             $params = [':login' => $login, ':email' => $email, ':password' => $password];
 
+            //выполнение
             $stmt = $pdo->prepare($sql);
             $stmt->execute($params);
             
