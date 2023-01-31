72ab234aa5c93fc6a5f897b7c8bc6608f4bb92ca
RinaBoni <brisovae@mail.ru>
2023-01-31 10:14:50 +0900

    эта хуета не работает

--- vendor/add_pet.php
+++ vendor/add_pet.php
@@ -13,22 +13,30 @@ echo $id_user;
 if(!empty($name) && !empty($weight) && !empty($activity)){
  
     
-        $sql = 'INSERT INTO `pets`( `id_user`, `name`, `weight`, `activity`) VALUES(:id_user, :name, :weight, :activity';
-        $params = [
-        
-            ':id_user' => $id_user,
-            ':name' => $name,
-            ':weight' => $weight,
-            ':activity' => $activity
-        ];
-
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute($params);
+    $sql = 'INSERT INTO `pets`( `id_user`, `name`, `weight`, `activity`) VALUES(:id_user, :name, :weight, :activity';
+    $params = [
     
-        //поскольку это транзакция, запись в бд добавляется только после commit
-        
-        echo 'Новый питомец успешно добавлен';
-        header('Location: ../profile.php');
+        //':id_user' => $_SESSION['user_id'],
+        'id_user' => $id_user,
+        'name' => $name,
+        'weight' => $weight,
+        'activity' => $activity
+    ];
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute($params);
+    // $sql = mysqli_query($stmt, 'INSERT INTO `pets`( `id_user`, `name`, `weight`, `activity`) 
+    //                             VALUES 
+    //                             ('{$_SESSION['user_id']}', '{$_POST['name']}', '{$_POST['weight']}', '{$_POST['activity']}')');
+
+    
+    // if ($sql) {
+    //     echo '<p>Данные успешно добавлены в таблицу.</p>';
+    //   } else {
+    //     echo '<p>Произошла ошибка: ' ;
+    //   }
+    echo 'Новый питомец успешно добавлен';
+    header('Location: ../profile.php');
 
 
 }else{
